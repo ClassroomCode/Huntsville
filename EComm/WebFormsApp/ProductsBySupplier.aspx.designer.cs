a04e8b8a0b417c3ff4d96ab384fa3ee065988a5b
Jason Bell <jbell@accelebrate.com>
2022-11-16 11:36:14 -0800

    UpdatePanel

--- EComm/WebFormsApp/ProductsBySupplier.aspx.designer.cs
+++ EComm/WebFormsApp/ProductsBySupplier.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace WebFormsApp
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+
+        /// <summary>
+        /// UpdatePanel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+
+        /// <summary>
         /// SupplierSqlDataSource control.
         /// </summary>
         /// <remarks>
@@ -58,5 +76,14 @@ namespace WebFormsApp
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListView ProductListView;
+
+        /// <summary>
+        /// UpdateProgress1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdateProgress UpdateProgress1;
     }
 }
